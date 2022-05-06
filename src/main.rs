048e0bb4f9795913a33cd38e837d52edff7d603b
Yash Karandikar <nerdstep710@gmail.com>
2022-05-06 11:02:22 -0500

    Add ability to query notion

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use serde_json::json;
 #[derive(Clone, Debug, Deserialize)]
 struct Config {
     discord: DiscordConfig,
+    notion: NotionConfig,
 }
 
 #[derive(Clone, Debug, Deserialize)]
@@ -11,6 +12,24 @@ struct DiscordConfig {
     webhook: String,
     username: Option<String>,
     avatar: Option<String>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct NotionConfig {
+    secret: String,
+    database: String,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct Page {
+    id: String,
+    last_edited_time: String,
+    url: String,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct Response {
+    results: Vec<Page>,
 }
 
 #[tokio::main]
@@ -23,11 +42,27 @@ async fn main() -> anyhow::Result<()> {
     });
 
     let client = reqwest::Client::new();
-    client
-        .post(config.discord.webhook)
-        .json(&body)
+
+    let resp = client
+        .post(format!(
+            "https://api.notion.com/v1/databases/{}/query",
+            config.notion.database
+        ))
+        .header("Content-Type", "application/json")
+        .header("Authorization", format!("Bearer {}", config.notion.secret))
+        .header("Notion-Version", "2022-02-22")
         .send()
+        .await?
+        .json::<Response>()
         .await?;
+
+    //client
+    //    .post(config.discord.webhook)
+    //    .json(&body)
+    //    .send()
+    //    .await?;
+
+    dbg!(&resp);
 
     Ok(())
 }
